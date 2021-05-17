b01f51a49c62b990fc521f84de2dc5db17429729
Isabelle Soupault <isabelle.soupault@gmail.com>
2021-05-17 13:58:05 +0200

    disparition du btn start au clic

---
+++ duckhunt.js
@@ -0,0 +1,6 @@
+
+function masquage() {
+    let container_Elt = document.getElementById('masquage');
+    container_Elt.classList.add("masquage");
+    container_Elt.classList.remove("visible");
+}
